f002e54404efa9f4c6b2b9ccdf808046885f33b8
vishal <vishal.kumar1145@gmail.com>
2016-07-31 22:48:56 +0530

    milestone 1 checkedin
    milestone 1 checkedin

--- js/functions.js
+++ js/functions.js
@@ -1,45 +1,94 @@
-app.directive('toogle',  ['$rootScope', function($rootScope) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            $(".nav_bar_btn").click(function(e) {
-        		$(".nav_bar_nav").slideToggle();
-    		});
-			$(window).resize(function(e) {
-                $(".nav_bar_nav").removeAttr("style");
-            });
-        }
-    };
-}]);
+function IsNull(val) {
+    if (val == '' || val == null || val === undefined)
+        return true;
+    else
+        return false;
+}
 
-app.directive('ngView',  ['$rootScope', function($rootScope) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var a = {
-                Android: function() {
-                    return navigator.userAgent.match(/Android/i);
-                },
-                BlackBerry: function() {
-                    return navigator.userAgent.match(/BlackBerry/i);
-                },
-                iOS: function() {
-                    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-                },
-                Opera: function() {
-                    return navigator.userAgent.match(/Opera Mini/i);
-                },
-                Windows: function() {
-                    return navigator.userAgent.match(/IEMobile/i);
-                },
-                any: function() {
-                    return a.Android() || a.BlackBerry() || a.iOS() || a.Opera() || a.Windows();
-                }
-            };
-            trueMobile = a.any();
-            if (null == trueMobile) {
-                var s = skrollr.init().refresh();
-            }
+function ReplaceNull(val) {
+    if (val == '' || val == null || val === undefined)
+        return '';
+    else
+        return val;
+}function CleanProfileUrl(url) {
+    if (IsNull(url))
+        return "images/duser.png";
+    else
+        return url;
+}function ValidateControl(ctrls) {
+
+    var isValid = true;
+    
+    for (var i = 0; i < ctrls.length; i++) {
+        var c = $("#" + ctrls[i])
+        if (IsNull(c.val())) {
+            isValid = false;
+            //c.toggleClass("has-errors");
+            c.css("border", "1px solid red");
         }
-    };
+        else {
+            c.css("border", "1px solid #ffd98c");
+        }
+    }
+
+    return isValid;
+
+}
+function PrepareRequestForMail(prcid, TO, CC, From, Subject, Body, DisplayName) {
+    var inputData = {};
+    inputData.PRCID = prcid;
+    var dataArray = [];
+    dataArray.push({ Key: "TO", Value: TO });
+    dataArray.push({ Key: "CC", Value: CC });
+    dataArray.push({ Key: "From", Value: From });
+    dataArray.push({ Key: "Subject", Value: Subject });
+    dataArray.push({ Key: "Body", Value: Body });
+    dataArray.push({ Key: "DisplayName", Value: DisplayName });
+    inputData.DataArray = dataArray;
+    console.log(inputData);
+    return inputData;
+}
+function initparallax() {
+    var a = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return a.Android() || a.BlackBerry() || a.iOS() || a.Opera() || a.Windows();
+        }
+    };
+    trueMobile = a.any();
+    if (null == trueMobile) {
+        var s = skrollr.init();
+    }
+    	 
+}
+$(document).ready(function() {
+    initparallax();
+});
+
+app.directive('toogle',  ['$rootScope', function($rootScope) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            $(".nav_bar_btn").click(function(e) {
+        		$(".nav_bar_nav").slideToggle();
+    		});
+			$(window).resize(function(e) {
+                $(".nav_bar_nav").removeAttr("style");
+            });
+        }
+    };
 }]);
