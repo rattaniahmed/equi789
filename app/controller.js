fdfe87499a306550c57204b47812759086311c15
vishal <vishal.kumar1145@gmail.com>
2016-10-04 20:23:17 -0700

    Implementing Image Pdf

--- app/controller.js
+++ app/controller.js
@@ -1623,6 +1623,95 @@ app.controller('RideDetailController', function MyCtrl($scope, $location, $fireb
 
 
 
+    //$scope.toDataUrl = function (url, callback) {
+    //    var xhr = new XMLHttpRequest();
+    //    //xhr.responseType = 'blob';
+    //    xhr.onload = function () {
+    //        var reader = new FileReader();
+    //        reader.onloadend = function () {
+    //            callback(reader.result);
+    //        }
+    //        reader.readAsDataURL(xhr.response);
+    //    };
+    //    xhr.open('GET', url, true);
+    //    xhr.send();
+    //}
+
+    //$scope.toDataUrl('http://www.w3schools.com/howto/img_fjords.jpg', function (base64Img) {
+    //    $scope.urldata = base64Img;
+    //    console.log(base64Img);
+    //});
+
+    //$("img").one("load", function () {
+    //    // do stuff
+    //}).each(function () {
+    //    if (this.complete) $(this).load();
+    //});
+
+    var canvas = document.getElementById('myCanvas');
+    var context = canvas.getContext('2d');
+    var imgData = canvas.toDataURL("http://www.w3schools.com/howto/img_fjords.jpg", 1.0);
+
+    $scope.PrintPDF = function () {
+
+
+        
+
+            var pdf = new jsPDF('p', 'pt', 'letter');
+
+            // source can be HTML-formatted string, or a reference
+            // to an actual DOM element from which the text will be scraped.
+            source = $('#HorseRideDetailNew')[0];
+
+            // we support special element handlers. Register them with jQuery-style 
+            // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+            // There is no support for any other type of selectors 
+            // (class, of compound) at this time.
+            specialElementHandlers = {
+                // element with id of "by pass" - jQuery style selector
+                '#bypassme': function (element, renderer) {
+                    // true = "handled elsewhere, bypass text extraction"
+                    return true
+                }
+            };
+            margins = {
+                top: 80,
+                bottom: 60,
+                left: 10,
+                width: 700
+            };
+            // all coords and widths are in jsPDF instance's declared units
+            // 'inches' in this case
+            pdf.fromHTML(
+            source, // HTML string or DOM elem ref.
+            margins.left, // x coord
+            margins.top, { // y coord
+                'width': margins.width, // max width of content on PDF
+                'elementHandlers': specialElementHandlers
+            },
+
+            function (dispose) {
+                // dispose: object with X, Y of the last line add to the PDF 
+                //          this allow the insertion of new lines after html
+                pdf.save('Test.pdf');
+            }, margins);
+    }
+
+
+
+  
+    //$scope.PrintHorsePDF = function () {
+    //    html2canvas(document.getElementById("HorseRideDetailNew"), {
+    //        onrendered: function (canvas) {
+    //            var image = canvas.toDataURL("image/png");
+            
+    //            var doc = new jsPDF();
+    //            doc.addImage(image, 'JPEG', 20, 20);
+             
+    //            doc.save('MYTest.pdf');
+    //        }
+    //    });
+    //}
 
 
     $scope.DeleteRide = function (id) {
