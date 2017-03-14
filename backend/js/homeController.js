635492782eb168a133bdf47d7bc5bb8debf71436
vishal <vishal.kumar1145@gmail.com>
2017-03-14 13:13:30 +0530

    remove the today and yesterday link

--- backend/js/homeController.js
+++ backend/js/homeController.js
@@ -86,10 +86,11 @@
             $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
         };
 
+//        'Today': [moment(), moment()],
+//'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+
         var optionSet1 = {                     
             ranges: {
-                'Today': [moment(), moment()],
-                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
                 'Last 7 Days': [moment().subtract(6, 'days'), moment()],
                 'Last 30 Days': [moment().subtract(29, 'days'), moment()],
                 'This Month': [moment().startOf('month'), moment().endOf('month')],
