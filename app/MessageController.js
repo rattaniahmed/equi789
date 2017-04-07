aa664e90acca3b6921bbe40a4dce0ac8102911dc
vishal <vishal.kumar1145@gmail.com>
2017-04-07 10:09:08 +0530

    icon

--- app/MessageController.js
+++ app/MessageController.js
@@ -1,11 +1,10 @@
 ﻿app.controller('MessageController', function MyCtrl($http, $scope, $location, $firebaseObject, $firebaseArray, firebaseService, storageService, blockUI, Socialshare) {
-    var ref = firebaseService.FIREBASEENDPOINT();   // new Firebase(firebaseService.USERSENDPOINT);
+    var ref = firebaseService.FIREBASEENDPOINT();  
     $scope.user = storageService.getObject("CU");
     if (!($scope.user)) {
         alert("Please Login to view the Messages");
         return;
     }
-    console.log("MessageController");
 
     $scope.Logout = function () {
         storageService.setObject("CU", null);
@@ -14,7 +13,7 @@
     $scope.ShowMessages = [];
     $scope.RefreshMessages = function () {
         $scope.ShowMessages = [];
-        //for (var i = 0; i < $scope.UserOrg.length; i++) {
+       
 
         for (var mcounter in $scope.AllMessages) {
             if (moment(dateFormat(new Date(), 'mm/dd/yyyy')).isSame(moment($scope.AllMessages[mcounter].ExpirationDate)) == true || (moment($scope.AllMessages[mcounter].ExpirationDate).isBefore(moment(dateFormat(new Date(), 'mm/dd/yyyy')))) == false) {
@@ -32,26 +31,26 @@
             }
         }
        
-       // $scope.$apply();
+        $scope.reverse = true;
 
     }
     $scope.openLink = function (msg) {
-       // window.location.href = url, '_blank';
+      
        
         var a = msg.Id;
         firebase.database().ref('/Content/Messages/' + a).once('value', function (msgsnapshot) {
-          // var uid = "sadsds";
+         
            var msgObject = msgsnapshot.val();
            if (msgObject.ReadBy) {
                var findid = _.contains(msgObject.ReadBy, $scope.user.Details.$id);
                if (!findid) {
                    msgObject.ReadBy.push($scope.user.Details.$id);
-                   //msgObject.Read = parseInt(msgObject.Read) + 1;
+                   
                }
            }
            else {
                msgObject.ReadBy = [$scope.user.Details.$id];
-               //msgObject.Read = 1;
+             
            }
 
            if (msgObject.ReadBy) {
@@ -59,25 +58,24 @@
            } else
                msgObject.ReadBy = 1;
 
-            //msgsnapshot.ref().update(msgObject);
+           
            firebase.database().ref('/Content/Messages/' + a).set(msgObject);
-            // firebase.database().ref().child('/Content/Messages/' + a).set(msgObject);
+           
         });
         window.open(msg.Embeddedlink, '_blank');
     }
     $scope.AllMessages = [];
     $scope.UserOrg= [];            
 
-    //var ref = firebaseService.FIREBASEENDPOINT();   
+    
     $scope.messages = $firebaseArray(ref.child('Content').child('Messages'));
-   //$scope.messages.$loaded().then(function (dataArray) {
+  
     firebase.database().ref('/Content/Messages').on('value', function (msgsnapshot) {
 
 
-        console.log(msgsnapshot.val());
+       
         $scope.AllMessages = msgsnapshot.val();
-        //$scope.messages = $scope.AllMessages;
-       // $scope.$apply();
+       
         $scope.RefreshMessages();
         for (var i in $scope.user.Details.horse_ids) {
             firebase.database().ref('/horses/' + i).on('value', function (snapshot) {
